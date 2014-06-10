aa551cb7adc91d1ccc9cba5904f526fc41da6b29
VanessaMaigne <vmaigne@gmail.com>
2014-06-10 18:48:41 +0200

    Evol bootstrap

--- body_rc.php
+++ body_rc.php
@@ -1,96 +1,19 @@
 <div id="pageWrapper">
+    <div class="title">REgional Carbon Cycle Assessment and Processes</div>
 
-    <div class="rowWrapper">
-        <div class="DCObject">
-            <h2>Net Primary Production</h2>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-6 col-md-6">
+                <h3>Choropleth Map</h3>
 
-<!--            <div id="npp-gpp-chart"></div>-->
-        </div>
-
-        <div class="DCObject">
-            <div class="">
-                <h2>Choropleth Map</h2>
-
-<!--                <div id="map-chart"></div>-->
-            </div>
-            <div id="reset">Reset All</div>
-        </div>
-
-        <div class="DCObject">
-            <h2>Heterotrophic Respiration</h2>
-
-<!--            <div id="hr-chart"></div>-->
-        </div>
-
-        <div class="DCObject functionsPie">
-            <h2>Functions</h2>
-
-<!--            <div id="function-pie-chart"></div>-->
-        </div>
-
-    </div>
-
-    <div class="rowWrapper">
-        <div class="DCObject">
-            <h2>Flux</h2>
-
-<!--            <img src="img/CarbonBudget_maquette.jpg" width="800px"/>-->
-                        <div id="flux-chart"></div>
-        </div>
-
-        <div class="DCObject">
-            <h2>GPP / NPP</h2>
-
-            <div id="bubble-chart"></div>
-        </div>
-
-    </div>
-
-    <div class="rowWrapper">
-        <BR/> <BR/>
-
-        <div class="DCObject">
-            <h2>Data</h2>
-
-            <div id="data-count" class="dc-data-count dc-chart">
-                <span class="filter-count"></span> selected out of <span class="total-count"></span> records
-            </div>
-            <BR/><BR/>
-
-            <table id="data-table" class="table table-hover dc-data-table dc-chart">
-                <thead>
-                <tr class="header">
-                    <th>Carbon budget</th>
-                    <th>Value</th>
-                </tr>
-                </thead>
-            </table>
-        </div>
-
-        <div class="DCObject">
-            <div class="smallBar">
-                <h2>Land use change</h2>
-
-                <div id="landUse-chart"></div>
+                <div id="map-chart"></div>
             </div>
 
-            <div class="smallBar">
-                <h2>River export to ocean</h2>
+            <div class="col-xs-6 col-md-5 col-md-offset-1">
+                <h3>Functions</h3>
 
-                <div id="riverExport-chart"></div>
+                <div id="function-chart"></div>
             </div>
-
-            <div class="smallBar">
-                <h2>Logging</h2>
-
-                <div id="logging-chart"></div>
-            </div>
-        </div>
-
-        <div class="DCObject functionsRow">
-            <h2>Functions</h2>
-
-            <div id="function-chart"></div>
         </div>
     </div>
 
--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<HTML>
+<!DOCTYPE html>
+<HTML lang="en">
 
 <!--
  ##########################################################################
 
- Patrick.Brockmann@lsce.ipsl.fr for Global Carbon Atlas
  Vanessa.Maigne@lsce.ipsl.fr    for Global Carbon Atlas
+ Patrick.Brockmann@lsce.ipsl.fr for Global Carbon Atlas
  Pascal.Evano@lsce.ipsl.fr      for Global Carbon Atlas
 
  PLEASE DO NOT COPY OR DISTRIBUTE WITHOUT PERMISSION
@@ -14,12 +15,15 @@
 
 <HEAD>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>GCA Reccap</title>
     <link rel="icon" href="img/globe.png" type="image/png">
 
-    <link rel="stylesheet" type="text/css" href="js/dc.js-master/web/css/bootstrap.min.css">
+<!--    <link rel="stylesheet" type="text/css" href="js/dc.js-master/web/css/bootstrap.min.css">-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-3.1.1-dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="js/dc.js-master/web/css/dc.css">
-    <link rel="stylesheet" type="text/css" href="css/globalCarbonAtlas.css">
+<!--    <link rel="stylesheet" type="text/css" href="css/globalCarbonAtlas.css">-->
     <link rel="stylesheet" type="text/css" href="css/RCInterface.css">
 
     <script type="text/javascript" src="js/library/jquery-1.9.1.js"></script>

5361e6c62f83ef0f516082a7aae5ae41cc98466d
VanessaMaigne <vmaigne@gmail.com>
2014-07-28 14:32:26 +0200

    Uncertainty image

--- body_rc.php
+++ body_rc.php
@@ -31,8 +31,8 @@
                     <div id="regionUnActive" class="toolButton"><img src="img/Globe_Disconnected.png" width="35px" title="See only one region"/></div>
                     <div id="regionActive" class="toolButton"><img src="img/Globe_RegionConnected.png" width="35px" title="See multiple regions"/></div>
 
-                    <div id="uncertaintyDisable" class="toolButton"><img src="img/uncertainty_disable.png" width="40px" title="Hide uncertainty"/></div>
-                    <div id="uncertainty" class="toolButton"><img src="img/uncertainty.png" width="40px" title="Display uncertainty"/></div>
+                    <div id="uncertaintyDisable" class="toolButton"><img src="img/uncertainty_disable2.png" width="40px" title="Hide uncertainty"/></div>
+                    <div id="uncertainty" class="toolButton"><img src="img/uncertainty2.png" width="40px" title="Display uncertainty"/></div>
                 </div>
 
                 <div id="mapChartAndComment">
